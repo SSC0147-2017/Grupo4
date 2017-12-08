115c9a97160608a1efcc82c2e586d974f4b9ecef
Hiero Martinelli <hieromartinelli@gmail.com>
2017-12-08 00:43:59 -0200

    Revert "Scripts não utilizados deletados"
    This reverts commit 8e4cbeaf0e7a18f954dfb1e329cc14bf3366e355.

---
+++ Scripts/camera.gd
@@ -0,0 +1,20 @@
+extends Camera
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+#func _input(event):
+#	if event.type == InputEvent.KEY:
+#		if event.is_action_pressed("ui_camera_right"):
+#			self.set_pause(true)
+#			get_tree().get_root().get_node("Battle/Camera2").set_pause(false)
+#			get_tree().get_root().get_node("Battle/Camera2").make_current()
+#		if event.is_action_pressed("ui_camera_left"):
+#			self.set_pause(true)
+#			get_tree().get_root().get_node("Battle/Camera4").set_pause(false)
+#			get_tree().get_root().get_node("Battle/Camera4").make_current()
+
+func _ready():
+#	set_process_input(true)
+	pass
---
+++ Scripts/camera2.gd
@@ -0,0 +1,16 @@
+extends Camera
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+	
+#func _input(event):
+#	if event.is_action_pressed("ui_camera_left"):
+#		get_node("Camera1").set_current()
+#	if event.is_action_pressed("ui_camera_right"):
+#		get_node("Camera3").set_current()
