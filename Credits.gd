c13a22697cb1ea7949c3ec2aa8149a4db68c486d
Hiero Martinelli <hieromartinelli@gmail.com>
2017-12-10 01:36:24 -0200

    Botão para sair dos créditos adicionado

---
+++ Credits.gd
@@ -0,0 +1,15 @@
+extends ColorFrame
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+
+func _on_MenuButton_pressed():
+	get_tree().change_scene("res://MainMenu.tscn")
+	pass # replace with function body
