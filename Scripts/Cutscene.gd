3697254841d2ad4583ca5b530f8170b975ea89d4
Hiero Martinelli <hieromartinelli@gmail.com>
2017-12-13 20:55:39 -0200

    Adicionado recurso de backgrounds no sistema de cutscenes

--- Scripts/Cutscene.gd
+++ Scripts/Cutscene.gd
@@ -21,42 +21,36 @@ func _on_Next_pressed():
 		readConfig(file, nextLine)
 	elif file.eof_reached():
 		file.close()
+		var scene = preload("res://Battle.tscn")
 		get_tree().change_scene("res://Battle.tscn")
 	else:
 		readNewText(file, nextLine)
 	pass
 
 func readNewText(var file, var nextLine):
-	print(str("TEXT: " + nextLine[0]))
+	#print(str("TEXT: " + nextLine[0]))
 	get_tree().get_root().get_node("Background/TextPanel/Text").set_text(str(nextLine[0]))
 	position = file.get_pos()
-	print("TEXT: " + str(position))
+	#print("TEXT: " + str(position))
 	pass
 	
 func readConfig(var file, var nextLine):
-	print(str("CONFIG: " + nextLine[0]))
+	#print(str("CONFIG: " + nextLine[0]))
 	while(!str(nextLine[0]).match("end_config")):
 		if str(nextLine[0]).match("left"):
 			nextLine = file.get_csv_line(delimiter)
-			print("2nd: " + str(nextLine[0]))
-			#if str(nextLine[0]).match("00"):
-			get_tree().get_root().get_node("Background/char1").set_texture(load("res://Source//Cutscenes//" + nextLine[0] + ".png"))
-			#if str(nextLine[0]).match("01"):
-			#	get_tree().get_root().get_node("Background/char1").set_texture(load("res://Source//Cutscenes//01.png"))
-			#if str(nextLine[0]).match("02"):
-			#	get_tree().get_root().get_node("Background/char1").set_texture(load("res://Source//Cutscenes//02.png"))
+			#print("2nd: " + str(nextLine[0]))
+			get_tree().get_root().get_node("Background/char1").set_texture(load("res://Source//Cutscenes//sprites//" + nextLine[0] + ".png"))
 		if str(nextLine[0]).match("right"):
 			nextLine = file.get_csv_line(delimiter)
-			print("3rd: " + str(nextLine[0]))
-			#if str(nextLine[0]).match("00"):
-			get_tree().get_root().get_node("Background/char2").set_texture(load("res://Source//Cutscenes//" + nextLine[0] + ".png"))
-			#if str(nextLine[0]).match("01"):
-			#	get_tree().get_root().get_node("Background/char2").set_texture(load("res://Source//Cutscenes//01.png"))
-			#if str(nextLine[0]).match("02"):
-			#	get_tree().get_root().get_node("Background/char2").set_texture(load("res://Source//Cutscenes//02.png"))
+			#print("3rd: " + str(nextLine[0]))
+			get_tree().get_root().get_node("Background/char2").set_texture(load("res://Source//Cutscenes//sprites//" + nextLine[0] + ".png"))
+		if str(nextLine[0]).match("bg"):
+			nextLine = file.get_csv_line(delimiter)
+			get_tree().get_root().get_node("Background").set_texture(load("res://Source//Cutscenes//bg//" + nextLine[0] + ".png"))
 		nextLine = file.get_csv_line(delimiter)
 	nextLine = file.get_csv_line(delimiter)
 	position = file.get_pos()
-	print("CONFIG: " + str(position))
+	#print("CONFIG: " + str(position))
 	readNewText(file, nextLine)
 	pass
