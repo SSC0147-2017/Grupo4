3082cf16e4a5c51765d9d9c4ad063628496abfd9
Hiero Martinelli <hieromartinelli@gmail.com>
2017-12-13 11:15:51 -0200

    Sistema de cutscenes pronto

---
+++ Scripts/Cutscene.gd
@@ -0,0 +1,62 @@
+extends Panel
+
+var position = 0
+var delimiter = "|"
+
+func _ready():
+	var file = File.new()
+	file.open("res://Text//Aqui.txt", file.READ)
+	file.seek(position)
+	var nextLine = file.get_csv_line(delimiter)
+	readConfig(file, nextLine)
+	file.close()
+	pass
+
+func _on_Next_pressed():
+	var file = File.new()
+	file.open("res://Text//Aqui.txt", file.READ)
+	file.seek(position)
+	var nextLine = file.get_csv_line(delimiter)
+	if str(nextLine[0]).match("start_config"):
+		readConfig(file, nextLine)
+	elif file.eof_reached():
+		file.close()
+		get_tree().change_scene("res://Battle.tscn")
+	else:
+		readNewText(file, nextLine)
+	pass
+
+func readNewText(var file, var nextLine):
+	print(str("TEXT: " + nextLine[0]))
+	get_tree().get_root().get_node("Background/TextPanel/Text").set_text(str(nextLine[0]))
+	position = file.get_pos()
+	print("TEXT: " + str(position))
+	pass
+	
+func readConfig(var file, var nextLine):
+	print(str("CONFIG: " + nextLine[0]))
+	while(!str(nextLine[0]).match("end_config")):
+		if str(nextLine[0]).match("left"):
+			nextLine = file.get_csv_line(delimiter)
+			print("2nd: " + str(nextLine[0]))
+			if str(nextLine[0]).match("00"):
+				get_tree().get_root().get_node("Background/char1").set_texture(load("res://Source//Cutscenes//00.png"))
+			if str(nextLine[0]).match("01"):
+				get_tree().get_root().get_node("Background/char1").set_texture(load("res://Source//Cutscenes//01.png"))
+			if str(nextLine[0]).match("02"):
+				get_tree().get_root().get_node("Background/char1").set_texture(load("res://Source//Cutscenes//02.png"))
+		if str(nextLine[0]).match("right"):
+			nextLine = file.get_csv_line(delimiter)
+			print("3rd: " + str(nextLine[0]))
+			if str(nextLine[0]).match("00"):
+				get_tree().get_root().get_node("Background/char2").set_texture(load("res://Source//Cutscenes//00.png"))
+			if str(nextLine[0]).match("01"):
+				get_tree().get_root().get_node("Background/char2").set_texture(load("res://Source//Cutscenes//01.png"))
+			if str(nextLine[0]).match("02"):
+				get_tree().get_root().get_node("Background/char2").set_texture(load("res://Source//Cutscenes//02.png"))
+		nextLine = file.get_csv_line(delimiter)
+	nextLine = file.get_csv_line(delimiter)
+	position = file.get_pos()
+	print("CONFIG: " + str(position))
+	readNewText(file, nextLine)
+	pass
