f42a2848f4d9af4c1d63846d1c9c199d269cb2db
Hiero Martinelli <hieromartinelli@gmail.com>
2017-12-14 14:45:57 -0200

    Cutscenes antes e depois da primeira batalha terminadas

--- Scripts/BeforeBattle1Cut.gd
+++ Scripts/BeforeBattle1Cut.gd
@@ -5,7 +5,7 @@ var delimiter = "|"
 
 func _ready():
 	var file = File.new()
-	file.open("res://Text//sampleText.txt", file.READ)
+	file.open("res://Text//Before.txt", file.READ)
 	file.seek(position)
 	var nextLine = file.get_csv_line(delimiter)
 	readConfig(file, nextLine)
@@ -14,7 +14,7 @@ func _ready():
 
 func _on_Next_pressed():
 	var file = File.new()
-	file.open("res://Text//sampleText.txt", file.READ)
+	file.open("res://Text//Before.txt", file.READ)
 	file.seek(position)
 	var nextLine = file.get_csv_line(delimiter)
 	if str(nextLine[0]).match("start_config"):
@@ -28,29 +28,30 @@ func _on_Next_pressed():
 	pass
 
 func readNewText(var file, var nextLine):
-	#print(str("TEXT: " + nextLine[0]))
+	print(str("TEXT: " + nextLine[0]))
 	get_tree().get_root().get_node("Background/TextPanel/Text").set_text(str(nextLine[0]))
 	position = file.get_pos()
-	#print("TEXT: " + str(position))
+	print("TEXT: " + str(position))
 	pass
 	
 func readConfig(var file, var nextLine):
-	#print(str("CONFIG: " + nextLine[0]))
+	print(str("CONFIG: " + nextLine[0]))
 	while(!str(nextLine[0]).match("end_config")):
 		if str(nextLine[0]).match("left"):
 			nextLine = file.get_csv_line(delimiter)
-			#print("2nd: " + str(nextLine[0]))
+			print("2nd: " + str(nextLine[0]))
 			get_tree().get_root().get_node("Background/char1").set_texture(load("res://Source//Cutscenes//sprites//" + nextLine[0] + ".png"))
 		if str(nextLine[0]).match("right"):
 			nextLine = file.get_csv_line(delimiter)
-			#print("3rd: " + str(nextLine[0]))
+			print("3rd: " + str(nextLine[0]))
 			get_tree().get_root().get_node("Background/char2").set_texture(load("res://Source//Cutscenes//sprites//" + nextLine[0] + ".png"))
 		if str(nextLine[0]).match("bg"):
 			nextLine = file.get_csv_line(delimiter)
+			print(str(get_tree().get_root().get_node("Background").set_texture(load("res://Source//Cutscenes//bg//" + nextLine[0] + ".png"))))
 			get_tree().get_root().get_node("Background").set_texture(load("res://Source//Cutscenes//bg//" + nextLine[0] + ".png"))
 		nextLine = file.get_csv_line(delimiter)
 	nextLine = file.get_csv_line(delimiter)
 	position = file.get_pos()
-	#print("CONFIG: " + str(position))
+	print("CONFIG: " + str(position))
 	readNewText(file, nextLine)
 	pass
